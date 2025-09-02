e018c58e0646b231eeb18f52d6f8c70d902de3c4
Adnan Abbas Shah <106626201+adnanabbasshah@users.noreply.github.com>
2025-09-02 21:34:23 +0500

    app5.py

--- app5.py
+++ app5.py
@@ -115,7 +115,7 @@ if uploaded_files:
             panel.paste(im, (x_offset, y_offset))
 
         # Preview in app
-        st.image(panel, caption="Panel Preview", width=True)
+        st.image(panel, caption="Panel Preview", width=stretch)
 
         # Export
         buffered = io.BytesIO()
@@ -165,4 +165,5 @@ with st.sidebar:
         ''',
         unsafe_allow_html=True
     )
+
 
